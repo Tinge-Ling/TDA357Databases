b34e0376d8114360d014bfcb2c0f6d5cdf8bf344
Tinge-Ling <miss_amtl@live.com>
2016-02-25 15:04:12 +0100

    final fix for trigger1

--- lab4/testtriggers.sql
+++ lab4/testtriggers.sql
@@ -1,1 +1,18 @@
-# Testing values 
+# Testing values for trigger, registeringStudent
+
+--Student wants to register to a course that doesn't need any prerequisites -> should work
+INSERT INTO Registrations (coursecode, studentid) VALUES ('AIC692','4141414141');
+
+--Student has all the prerequisites, not in the lists, have not passed -> should work
+INSERT INTO Registrations (coursecode, studentid) VALUES ('AER682','3131313131');
+
+
+--Student has not the prerequisites -> raise exception 1
+INSERT INTO Registrations (coursecode, studentid) VALUES ('AER682','1212121212');
+
+--Student has the prerequisites, but is already in either of lists -> raise exception 2
+INSERT INTO Registrations (coursecode, studentid) VALUES ('MVV395','3232323232');
+
+
+--Student has the prerequisites, not in the lists but has already passed the course -> raise exception 3
+INSERT INTO Registrations (coursecode, studentid) VALUES ('TKG431','1313131313');
--- lab4/triggers.sql
+++ lab4/triggers.sql
@@ -9,21 +9,34 @@ EXECUTE PROCEDURE registeringStudent();
 # Function for registering Student
 CREATE OR REPLACE FUNCTION registeringStudent() RETURNS TRIGGER AS $$
 BEGIN
-IF (SELECT EXISTS (SELECT 1 FROM passedcourses WHERE coursecode = (SELECT prereq FROM isprerequisite WHERE coursecode=new.coursecode AND passedcourses.id=new.studentid)))
-	THEN IF(SELECT exists(SELECT 1 FROM limitedcourse WHERE coursecode=new.coursecode))
-		THEN IF(SELECT count(*) FROM registrations WHERE coursecode=new.coursecode AND status='registered')<=(select maxparticipants from limitedcourse where coursecode=new.coursecode)
-				THEN INSERT INTO isregistered VALUES (new.coursecode, new.studentid);
-					return new;
-				ELSE INSERT INTO iswaiting VALUES(current_date, new.studentid,new.coursecode);
-					return new;
-				END IF;
-		ELSE INSERT INTO isregistered VALUES (new.coursecode, new.studentid);
-			return new;
-		END IF;
-	ELSE RAISE EXCEPTION 'Lack of prerequisites';
-	END IF;
-END;
 
+		-- True if the course needs prerequisites
+		-- True if the student has NOT passed the prerequisite course
+		IF  (SELECT EXISTS (SELECT 1 FROM isprerequisite WHERE coursecode =new.coursecode) AND (SELECT NOT EXISTS (SELECT 1 FROM passedcourses WHERE coursecode = (SELECT prereq FROM isprerequisite WHERE coursecode=new.coursecode AND passedcourses.id=new.studentid)))) 
+				THEN RAISE EXCEPTION 'The student is lack of prerequisites.';
+		-- True if the student exists in the waiting or registered-list
+		ELSEIF (SELECT EXISTS (SELECT 1 FROM registrations WHERE studentid=new.studentid AND coursecode=new.coursecode))	
+				THEN RAISE EXCEPTION 'The student is already registered or on the waiting list.';
+		-- True if student exists in the passedcourses for that course
+		ELSEIF (SELECT EXISTS (SELECT 1 FROM passedcourses WHERE id=new.studentid AND coursecode=new.coursecode)) 
+				THEN RAISE EXCEPTION 'The student has already passed the course.';	 
+		ELSE 
+		-- True if the course has a limited number of participants
+			IF(SELECT exists(SELECT 1 FROM limitedcourse WHERE coursecode=new.coursecode))
+				-- checks if the number of registered students in the course is less to maximum number of participants
+			THEN IF(SELECT count(*) FROM registrations WHERE coursecode=new.coursecode AND status='registered')<(select maxparticipants from limitedcourse where coursecode=new.coursecode)
+						-- if yes, insert the student to isregistered-list
+					THEN INSERT INTO isregistered VALUES (new.coursecode, new.studentid);
+							return new;
+						-- if no, put the student into the waiting list
+					ELSE INSERT INTO iswaiting VALUES(current_date, new.studentid,new.coursecode);
+							return new;
+					END IF;
+				-- if the course has no limited number of participants		
+			ELSE INSERT INTO isregistered VALUES (new.coursecode, new.studentid);
+					return new;
+			END IF;
+		END IF;
+END;
 $$ LANGUAGE 'plpgsql' ;
 
-#
