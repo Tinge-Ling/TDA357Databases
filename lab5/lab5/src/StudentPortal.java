35723c9c7beb872ced8a00ceae35705e8be4b158
Rasmus Davidsson <ralle.davidsson@gmail.com>
2016-03-03 23:01:08 +0100

    added information method

--- lab5/lab5/src/StudentPortal.java
+++ lab5/lab5/src/StudentPortal.java
@@ -77,9 +77,83 @@ public class StudentPortal {
      * - the mandatory courses that the student has yet to read.
      * - whether or not the student fulfills the requirements for graduation
      */
-    static void getInformation(Connection conn, String student) throws SQLException {
+    static void getInformation(Connection conn, String student) throws SQLException
+    {
+        String studentQuery = "SELECT name,spname FROM Student WHERE id = '"+student+"'";
+        String branchQuery = "SELECT bname FROM hasbranch WHERE studentid = '"+student+"'";
+        String readCoursesQuery = "SELECT c.coursename,c.code,fc.grade,fc.creditpoints FROM Course AS c, finishedcourses AS fc " +
+                "WHERE fc.id = '"+student+"' AND fc.coursecode = c.code";
+        String registeredCoursesQuery = "SELECT c.coursename,c.code,reg.status FROM Course AS c, registrations AS reg " +
+                "WHERE reg.studentid = '"+student+"' AND reg.coursecode = c.code";
+        String positionQuery = "SELECT cqp.position FROM coursequeuepositions AS cqp WHERE cqp.studentid = '"+student+"'";
+        String pathToGradQuery = "SELECT ptg.nbrofseminar, ptg.mathcredits, ptg.researchcredits, ptg.totalcreditpoints, " +
+                "ptg.isqualifiedtograduate " +
+                "FROM pathtograduation AS ptg " +
+                "WHERE ptg.id = '"+student+"'";
 
+
+        Statement myStatement = conn.createStatement();
+        Statement mS = conn.createStatement();
+        ResultSet res = myStatement.executeQuery(studentQuery);
+        ResultSet res2;
+
+
+        if(res.next()) {
+            System.out.print("Information for student " + student.substring(0, 6) + "-" + student.substring(6) + "\n");
+            System.out.println("------------------");
+            System.out.println("Name: " + res.getString(1));
+            System.out.println("Line: " + res.getString(2));
+            res.close();
+        }
+
+        res = myStatement.executeQuery(branchQuery);
+        if(res.next()) {
+            System.out.println("Branch: " + res.getString(1));
+            res.close();
+        }
+
+        System.out.print("\nRead Courses (name (code), credits : grade): \n");
+        res = myStatement.executeQuery(readCoursesQuery);
+
+        while(res.next()){
+            System.out.print(res.getString(1)+" ("+res.getString(2)+"), "+ res.getString(4)+"p: " +res.getString(3)+"\n");
+
+        }
+        res.close();
+
+        res = myStatement.executeQuery(registeredCoursesQuery);
+        res2 = mS.executeQuery(positionQuery);
+        System.out.print("\nRegistered Courses (name (code), credits : status):\n");
+
+        while(res.next()){
+            if(res.getString(3).equals("waiting")){
+                if(res2.next()){
+                    System.out.print(res.getString(1)+" ("+res.getString(2)+"): "+ res.getString(3)+" as nr "+ res2.getString(1)+"\n");
+
+                }
+            }else {
+                System.out.print(res.getString(1)+" ("+res.getString(2)+"): "+ res.getString(3)+"\n");
+
+            }
+
+
+        }
+        res2.close();
+        res.close();
+        mS.close();
+
+        res = myStatement.executeQuery(pathToGradQuery);
+
+        if(res.next()) {
+            System.out.print("\nSeminar courses taken: " +res.getString(1)+ "\nMath credits taken: "+res.getString(2)+
+                    "\nResearch credits taken: "+res.getString(3)+"\nTotal credits taken: "+res.getString(4)+
+                    "\nFulfills the requirements for graduation: "+res.getString(5)+"\n");
+
+        }
+        res.close();
+        myStatement.close();
     }
+
 
     /* Register: Given a student id number and a course code, this function
      * should try to register the student for that course.
